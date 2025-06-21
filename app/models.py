d5b4899cfb78cbba1610a24977852a7f0f7c4436
harshareddy237 <harshareddyperla@07gmail.com>
2025-06-21 11:10:41 +0530

    first commit

---
+++ app/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+class Task(models.Model):
+    title=models.CharField(max_length=100)
+    descrption=models.TextField()
+    due_time=models.TimeField()
+    due_date=models.DateField()
+    completed=models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+    
